95ea4e0fc9e00fa7b87cdf7947bd37c944b6839d
leftover19 <vishwaskapoor27@gmail.com>
2024-04-05 15:37:43 +0530

    commit on 5 April

--- config.lua
+++ config.lua
@@ -25,17 +25,33 @@ lvim.plugins = {
         end,
     },
     {
-        table.insert(lvim.plugins, {
-            "zbirenbaum/copilot-cmp",
-            event = "InsertEnter",
-            dependencies = { "zbirenbaum/copilot.lua" },
-            config = function()
-                vim.defer_fn(function()
-                    require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-                    require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-                end, 100)
-            end,
-        })
+        "zbirenbaum/copilot-cmp",
+        event = "InsertEnter",
+        dependencies = { "zbirenbaum/copilot.lua" },
+        config = function()
+            vim.defer_fn(function()
+                require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+                require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+                -- Below config is required to prevent copilot overriding Tab with a suggestion
+                -- when you're just trying to indent!
+                local has_words_before = function()
+                    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+                    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") ==
+                    nil
+                end
+                local on_tab = vim.schedule_wrap(function(fallback)
+                    local cmp = require("cmp")
+                    if cmp.visible() and has_words_before() then
+                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                    else
+                        fallback()
+                    end
+                end)
+                lvim.builtin.cmp.mapping["<Tab>"] = on_tab
+            end, 100)
+        end,
+
     },
     {
         "karb94/neoscroll.nvim",
