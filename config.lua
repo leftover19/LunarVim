cd7fa40687af1bdfc4bf9d657ee0899352361d7d
leftover19 <vishwaskapoor27@gmail.com>
2024-11-02 10:05:06 +0530

    formatting update, 2 nov 2024, all working find, few updates, check :check

--- config.lua
+++ config.lua
@@ -38,7 +38,7 @@ lvim.plugins = {
                     if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
                     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
                     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") ==
-                    nil
+                        nil
                 end
                 local on_tab = vim.schedule_wrap(function(fallback)
                     local cmp = require("cmp")
